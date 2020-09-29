668cbf821a5deea01ee13bccadae43661af68125
Michael Howe <mike@olympiate.com>
2020-09-29 13:58:46 -0600

    complete django tutorial

--- polls/views.py
+++ polls/views.py
@@ -24,8 +24,9 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those
         set to be released in the future).
         """
-        return Question.objects.filter(pub_date__lts=timezone.now()) \
-                .order_by('-pub_date')[:5]
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()
+               ).order_by('-pub_date')[:5]
 
 # Extra Info
 # {{{
@@ -42,6 +43,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name =  'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
